7618eca7255a1d613b0899d913e1a0c4637f0fb6
kobaken <kentafly88@gmail.com>
2025-02-10 12:48:04 +0900

    step8 をdockerで動くように調整

--- step8-SSR/honox/vite.config.ts
+++ step8-SSR/honox/vite.config.ts
@@ -1,8 +1,38 @@
-import build from '@hono/vite-build'
+import build from '@hono/vite-build/bun'
 import honox from 'honox/vite'
 import { viteMockServe } from "vite-plugin-mock";
 import { defineConfig } from 'vite'
+import type { UserConfig } from "vite";
 
-export default defineConfig({
-  plugins: [honox(), build({}), viteMockServe()]
-})
+export default defineConfig(({ mode }) => {
+	let config: UserConfig;
+	if (mode === "client") {
+		config = {
+			build: {
+				rollupOptions: {
+					input: {
+						client: "./app/client.ts",
+					},
+					output: {
+						dir: "./dist",
+						entryFileNames: "assets/[name].js",
+						assetFileNames: "assets/[name].[ext]",
+					},
+				},
+				copyPublicDir: false,
+				minify: true,
+			},
+		};
+	} else {
+		config = {
+			plugins: [
+				honox(),
+				build({
+					minify: false,
+				}),
+				viteMockServe(),
+			],
+		};
+	}
+	return config;
+});
