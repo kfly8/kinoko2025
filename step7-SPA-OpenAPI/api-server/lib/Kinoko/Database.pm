24d8728fbba425a557917d732f801adaefc75a37
kobaken <kentafly88@gmail.com>
2025-02-28 08:36:19 +0900

    Update

--- step7-SPA-OpenAPI/api-server/lib/Kinoko/Database.pm
+++ step7-SPA-OpenAPI/api-server/lib/Kinoko/Database.pm
@@ -2,13 +2,31 @@ use v5.40;
 use utf8;
 use experimental qw(class);
 
-use Mojo::SQLite;
-
 class Kinoko::Database {
     field $dsn :param = $ENV{DATABASE_DSN};
     field $sqlite;
 
-    ADJUST {
+    method db() {
+        $sqlite //= $self->_build_sqlite();
+        $sqlite->db;
+    }
+
+    method select_posts() {
+        my $posts = $self->db->query('SELECT id, name, comment, timestamp FROM posts ORDER BY id DESC')->hashes;
+    }
+
+    method select_post_by_id($id) {
+        my $post = $self->db->query('SELECT id, name, comment, timestamp FROM posts WHERE id = ?', $id)->hash;
+    }
+
+    method create_post($post) {
+        return unless $post->{comment};
+        my $id = $self->db->insert(posts => $post)->last_insert_id;
+    }
+
+    method _build_sqlite() {
+        require Mojo::SQLite;
+
         $sqlite = Mojo::SQLite->new($dsn);
         $sqlite->db->dbh->do('PRAGMA journal_mode = WAL');
         $sqlite->db->dbh->do(q{
@@ -22,18 +40,7 @@ class Kinoko::Database {
         $sqlite->on(connection => sub($sqlite, $dbh) {
             $dbh->do('PRAGMA synchronous = NORMAL');
         });
-    }
 
-    method select_posts() {
-        my $posts = $sqlite->db->query('SELECT id, name, comment, timestamp FROM posts ORDER BY id DESC')->hashes;
-    }
-
-    method select_post_by_id($id) {
-        my $post = $sqlite->db->query('SELECT id, name, comment, timestamp FROM posts WHERE id = ?', $id)->hash;
-    }
-
-    method create_post($post) {
-        return unless $post->{comment};
-        my $id = $sqlite->db->insert(posts => $post)->last_insert_id;
+        $sqlite;
     }
 }
